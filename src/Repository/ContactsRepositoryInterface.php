10a5aa8c60e82fed5d93625382b54cbe47893806
AmrEzzat <31932378+Amr3zzat@users.noreply.github.com>
2021-10-08 17:31:54 +0200

    [DB] Add Find searched to repo

--- src/Repository/ContactsRepositoryInterface.php
+++ src/Repository/ContactsRepositoryInterface.php
@@ -7,6 +7,7 @@ namespace App\Repository;
 
 interface ContactsRepositoryInterface
 {
+    public function findAllPaginated(int $offset = 0, int $limit = null, string $searchKey = null);
 
     public function find(int $id);
 
